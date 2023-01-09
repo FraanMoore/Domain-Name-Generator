2e654a3a62636eec2bf75903396b4c3e8e628d98
FraanMoore <fraanmoore@gmail.com>
2023-01-09 23:00:27 +0000

    opcion interectiva del ejercicio

--- src/app.js
+++ src/app.js
@@ -7,5 +7,19 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  document.querySelector("#NombresDominio").innerHTML = generateNombres();
+};
+let generateNombres = () => {
+  let pronoun = ["Our", "the"];
+  let adj = ["big", "small", "great", "smelly"];
+  let sust = ["horse", "racoon", "dog", "cat"];
+
+  let pronounIndex = Math.floor(Math.random() * pronoun.length);
+  let adjIndex = Math.floor(Math.random() * adj.length);
+  let sustIndex = Math.floor(Math.random() * sust.length);
+
+  return (
+    pronoun[pronounIndex] + "" + adj[adjIndex] + "" + sust[sustIndex] + ".com"
+  );
 };
